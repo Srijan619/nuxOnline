24beadbe0990df11c267eaa788152f359036c837
srijanpersonal <chapssrijan@gmail.com>
2025-03-27 08:09:24 +0200

    added transition group to knob container for smooth transition

--- src/components/KnobContainer.vue
+++ src/components/KnobContainer.vue
@@ -24,29 +24,31 @@ const updateValue = (controlPane: number, value: number) => {
 
 <template>
   <div class="knob-container">
-    <template
-      v-for="knob in state.selectedEffectOption.knobs"
-      :key="`${knob.id}-${selectedEffectColor}-${knob.currentValue}`"
-    >
-      <TwoWaySwitch
-        v-if="knob && knob.range[0] === 0 && knob.range[1] === 1"
-        :id="knob.id"
-        :title="knob.title"
-        :activeColor="selectedEffectColor"
-        :initialValue="knob.currentValue === 1"
-        @update:value="(value) => updateValue(knob?.ctrl, value ? 1 : 0)"
-      />
-      <KnobControl
-        v-else
-        :title="knob.title"
-        :size="100"
-        :min="knob.range[0]"
-        :max="knob.range[1]"
-        :initialValue="knob?.currentValue"
-        @update:value="(value) => updateValue(knob?.ctrl, value)"
-        :sliderFillColor="selectedEffectColor"
-      />
-    </template>
+    <TransitionGroup name="knobs">
+      <template
+        v-for="knob in state.selectedEffectOption?.knobs"
+        :key="`${knob.id}-${selectedEffectColor}-${knob.currentValue}-`"
+      >
+        <TwoWaySwitch
+          v-if="knob && knob.range[0] === 0 && knob.range[1] === 1"
+          :id="knob.id"
+          :title="knob.title"
+          :activeColor="selectedEffectColor"
+          :initialValue="knob.currentValue === 1"
+          @update:value="(value) => updateValue(knob?.ctrl, value ? 1 : 0)"
+        />
+        <KnobControl
+          v-else
+          :title="knob.title"
+          :size="100"
+          :min="knob.range[0]"
+          :max="knob.range[1]"
+          :initialValue="knob?.currentValue"
+          @update:value="(value) => updateValue(knob?.ctrl, value)"
+          :sliderFillColor="selectedEffectColor"
+        />
+      </template>
+    </TransitionGroup>
   </div>
 </template>
 
@@ -57,5 +59,23 @@ const updateValue = (controlPane: number, value: number) => {
   gap: 1rem;
   width: 100%;
   margin-left: 1rem;
+}
+
+.knobs-move, /* apply transition to moving elements */
+.knobs-enter-active,
+.knobs-leave-active {
+  transition: all 0.5s ease;
+}
+
+.knobs-enter-from,
+.knobs-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.knobs-leave-active {
+  position: absolute;
 }
 </style>
