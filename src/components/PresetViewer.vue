530047b1aba8ce821af10851163e4e7afbe2f3da
srijanpersonal <chapssrijan@gmail.com>
2025-03-30 14:24:14 +0300

    initialize scene

--- src/components/PresetViewer.vue
+++ src/components/PresetViewer.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import EffectChain from "./EffectChain.vue";
-import PresetChange from "./PresetChange.vue";
-import PresetDetail from "./PresetDetail.vue";
-import Device from "./Device.vue";
 import KnobContainer from "./KnobContainer.vue";
-import Save from "./Save.vue";
 import EffectsGrid from "./EffectsGrid.vue";
+import Scene from "./Scene.vue";
+import PresetContainer from "./PresetContainer.vue";
 
 // 🎭 composables
 import { useNUXMidiController } from "../composables/useNUXMidiController";
@@ -15,13 +13,8 @@ const { state } = useNUXMidiController();
 
 <template>
   <div class="main-container">
-    <div class="preset-card">
-      <PresetDetail />
-      <!-- <Save /> -->
-      <PresetChange>
-        <Device />
-      </PresetChange>
-    </div>
+    <Scene />
+    <PresetContainer />
     <EffectChain />
     <KnobContainer />
     <EffectsGrid />
@@ -37,18 +30,5 @@ const { state } = useNUXMidiController();
   max-width: 99%;
   overflow-x: hidden;
   padding: 1rem;
-}
-
-.preset-card {
-  position: relative;
-  width: 20rem;
-  border-radius: 1.25rem;
-  padding: 1rem;
-  box-shadow: 0 6px 20px rgba(0, 0, 0, 0.4);
-  overflow: hidden;
-  isolation: isolate;
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
 }
 </style>
---
+++ src/components/Scene.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// 🎭 composables
+import { useNUXMidiController } from "../composables/useNUXMidiController";
+
+const { state } = useNUXMidiController();
+</script>
+
+<template>
+  <div class="scene-container">
+    <div class="scene">{{ JSON.stringify(state.currentPresetData.scene) }}</div>
+  </div>
+</template>
+
+<style scoped>
+.scene-container {
+  display: flex;
+}
+</style>
